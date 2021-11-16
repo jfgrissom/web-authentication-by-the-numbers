6e16f912466a9511b2c94771870b45d79df05f8a
jfgrissom <jfgrissom@gmail.com>
2021-11-16 15:01:42 -0800

    Added basic auth.

--- index.ts
+++ index.ts
@@ -1,7 +1,14 @@
 import express, { Application, Request, Response } from 'express'
+const basicAuth = require('express-basic-auth')
 
 const app: Application = express()
 const port = 3000
+
+app.use(
+  basicAuth({
+    users: { admin: 'supersecret' }
+  })
+)
 
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
