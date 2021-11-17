7ebf393dd840711e57328ba996d260c09e05919a
jfgrissom <jfgrissom@gmail.com>
2021-11-17 10:41:52 -0800

    Changed from express-basic-auth to basic-auth

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
 import express, { Application, Request, Response } from 'express'
-import basicAuth from 'express-basic-auth'
+import auth from 'basic-auth'
 
 const app: Application = express()
 const port = 3000
 
-app.use(
-  basicAuth({
-    users: { admin: 'supersecret' }
-  })
-)
+app.use((req, res, next) => {
+  let user = auth(req)
+
+  if (
+    user === undefined ||
+    user['name'] !== 'admin' ||
+    user['pass'] !== 'supersecret'
+  ) {
+    res.statusCode = 401
+    res.setHeader('WWW-Authenticate', 'Basic realm="Node"')
+    res.end('Unauthorized')
+  } else {
+    next()
+  }
+})
 
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
   return res.status(200).send({
