f01df3cebd28420b02562ec719ccb384501d44bf
jfgrissom <jfgrissom@gmail.com>
2021-11-17 09:15:18 -0800

    Add basic session support.

--- index.ts
+++ index.ts
@@ -1,12 +1,17 @@
 import express, { Application, Request, Response } from 'express'
+import session from 'express-session'
 
 const app: Application = express()
 const port = 3000
 
+const sessionOptions = {
+  secret: 'session secret that is not secret'
+}
+
+app.use(session(sessionOptions))
+
 app.get('/', async (req: Request, res: Response): Promise<Response> => {
-  return res.status(200).send({
-    message: "Hello World! I'm not authenticated."
-  })
+  return res.send(`Session ID: ${req.session.id}`)
 })
 
 app.listen(port)
