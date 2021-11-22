28fee7e5070d5de8f265c36ca4b49a38d5600822
jfgrissom <jfgrissom@gmail.com>
2021-11-22 11:36:17 -0800

    Working session with auth from e2e using PassportJS local strategy.

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express, { Application, NextFunction, Request, Response } from 'express'
 import passport from 'passport'
 import { Strategy } from 'passport-local'
 import session from 'express-session'
+import { flash } from 'express-flash-message'
 
 const app: Application = express()
 const port = 3000
@@ -10,6 +11,12 @@ interface iUser {
   id: number
   username: string
   password: string
+}
+
+// Middleware to secure routes that use this passport strategy.
+const authRequired = (req: Request, res: Response, next: NextFunction) => {
+  if (!req.isAuthenticated()) return res.redirect('/login')
+  next()
 }
 
 const users: iUser[] = [{ id: 1, username: 'admin', password: 'supersecret' }]
@@ -32,37 +39,50 @@ passport.use(
 )
 
 const sessionConfig = {
-  secret: 'Not Really A Secret'
+  secret: 'Not Really A Secret But It Should Be In Production'
 }
 
+// Enable parsing of data that is posted from the user login form to the login endpoint.
+app.use(express.urlencoded())
+app.use(express.json())
+
+// Setup the session
 app.use(session(sessionConfig))
 app.use(passport.initialize())
 app.use(passport.session())
 
+// Enable flash messages to display auth errors.
+app.use(flash())
+
 passport.serializeUser((user, done) => {
+  console.log(`User to be Serialized: ${JSON.stringify(user)}`)
   return done(null, user)
 })
 
-passport.deserializeUser((id: number, done: Function) => {
-  const user = users.find((user) => user.id === id)
-  if (!user) return done(null, false)
+passport.deserializeUser((user: iUser, done: Function) => {
+  console.log(`Passed User: ${JSON.stringify(user)}`)
 
-  return done(null, user)
+  const foundUser = users.find(
+    (user) => user.username === user.username && user.password === user.password
+  )
+
+  if (!foundUser) return done(null, false)
+
+  console.log(`Found User: ${JSON.stringify(foundUser)}`)
+  return done(null, foundUser)
 })
 
 app.get('/', (req: Request, res: Response) => {
-  if (res.user) {
-    console.log('Thar Be Users!')
-  }
   return res.send(`
-    Home | <a href="/login">Login</a>
+    Home | <a href="/login">Login</a> | <a href="/members">Members</a>
     <H1>Home</H1>
     <p>This is a public route. No Authentication is needed.</p>
-    <p>User: ${req.user}</p>
+    <p>User: ${JSON.stringify(req.user)}</p>
   `)
 })
 
-app.get('/login', (req: Request, res: Response, next: NextFunction) => {
+app.get('/login', async (req: Request, res: Response, next: NextFunction) => {
+  const errors = await req.consumeFlash('error')
   res.send(`
     <a href="/">Home</a>
     <H1>Login</H1>
@@ -79,6 +99,9 @@ app.get('/login', (req: Request, res: Response, next: NextFunction) => {
         <input type="submit" value="Log In"/>
       </div>
     </form>
+    Errors: ${errors} <br/>
+    SessionID: ${req.session.id} <br/>
+    User: ${JSON.stringify(req.user)}
   `)
 })
 
@@ -86,29 +109,21 @@ app.post(
   '/login',
   passport.authenticate('local', {
     successRedirect: '/members',
-    failureRedirect: '/auth-failed'
+    failureRedirect: '/login',
+    failureFlash: true
   })
 )
 
-app.get('/auth-failed', (req: Request, res: Response, next: NextFunction) => {
-  res.send(`
-    <a href="/">Home</a> | <a href="/login">Login</a>
-    <H1>Failed!</H1>
-    <p>How does it feel?</p>
-    SessionID: ${req.session.id}
-  `)
-})
-
 app.get(
   '/members',
-  passport.authenticate('local', {
-    successRedirect: '/members',
-    failureRedirect: '/auth-failed'
-  }),
+  authRequired,
   (req: Request, res: Response, next: NextFunction) => {
     res.send(`
-    <H1>Members Only!<H1/>
+    <a href="/">Home</a>
+    <H1>Members Only!</H1>
     <p>Welcome</p>
+    SessionID: ${req.session.id} <br/>
+    User: ${JSON.stringify(req.user)}
   `)
   }
 )
